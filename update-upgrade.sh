4a26231f62f798e026ced945654e7481dbc3a2f9
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-05-05 02:03:18 -0400

    Update update-upgrade.sh

--- update-upgrade.sh
+++ update-upgrade.sh
@@ -4,32 +4,32 @@
 logfile="/var/log/update-upgrade.log"
 
 #Check if the log file exists, and if not create it
-if [ ! -e "$logfile" ] ; then
-   touch "$logfile"
+if ! test -e $logfile; then
+  touch $logfile
 fi
 
 #Write the current date and time to the log file
-echo -e "\033[1mDate: $(date)\033[0m" >> "$logfile"
+echo "Date: $(date)" | tee -a $logfile
 
 #Write the results of the update to the log file
-echo -e "\033[1mUpdate Results:\033[0m" >> "$logfile"
-sudo apt update -y | tee -a "$logfile"
+echo "Update Results:" | tee -a $logfile
+sudo apt update | tee -a $logfile
 
 #Write the results of the upgrade to the log file
-echo -e "\033[1mUpgrade Results:\033[0m" >> "$logfile"
-sudo apt upgrade -y | tee -a "$logfile"
+echo "Upgrade Results:" | tee -a $logfile
+sudo apt upgrade -y | tee -a $logfile
 
 #Write the results of the autoremove to the log file
-echo -e "\033[1mAutoremove Results:\033[0m" >> "$logfile"
-sudo apt autoremove -y | tee -a "$logfile"
+echo "Autoremove Results:" | tee -a $logfile
+sudo apt autoremove -y | tee -a $logfile
 
 #Write the results of the autoclean to the log file
-echo -e "\033[1mAutoclean Results:\033[0m" >> "$logfile"
-sudo apt autoclean | tee -a "$logfile"
+echo "Autoclean Results:" | tee -a $logfile
+sudo apt autoclean | tee -a $logfile
 
-if [ -f /var/run/reboot-required ]; then
-  echo -e "\033[1mA reboot was needed on $(date)\033[0m" | tee -a "$logfile"
+if test -f /var/run/reboot-required; then
+  echo "A reboot was needed on $(date)" | tee -a $logfile
   sudo reboot
 else
-  echo -e "\033[1mNo reboot was needed on $(date)\033[0m" | tee -a "$logfile"
+  echo "No reboot was needed on $(date)" | tee -a $logfile
 fi
